9882b6af06a106267bdade56bc7d69fbf3b6fdeb
Ragavan01 <ragavanr08n2002@gmail.com>
2023-11-26 17:57:26 +0530

    first commit

--- src/Iqac/Iqac.jsx
+++ src/Iqac/Iqac.jsx
@@ -3,17 +3,22 @@ import React from 'react'
 import './Iqac.css'
 import Navbar from './Navbar/Navbar'
 import banner1 from './Assets/banner-1.webp'
-import banner2 from './Assets/banner-2.webp'
+import banner2 from './Assets/NAAC.jpg'
+import banner3 from './Assets/banner-2.webp'
+import banner4 from './Assets/banner-3.jpg'
 
 const iqac = () => {
   return (
     <>
       <Navbar />
-      <div className="container" style={{ marginTop: '50px' }}>
+      <div className="container" style={{ marginTop: '60px' }}>
         <div className="row mb-5 m-2">
+          <div className="col-lg-6 mt-5 ml-auto d-lg-none">
+            <img src={banner1} alt="Images" class="img-fluid" style={{ borderRadius: "15px" }} />
+          </div>
           <div className="col-lg-6 mt-5 ml-auto">
             <div className='text-start mb-4 heading'>
-            <h3 className=" headingtext">About</h3>
+              <h3 className=" headingtext">About</h3>
             </div>
             <p className="text-start">SETHU EDUCATIONAL TRUST established SETHU INSTITUTE OF TECHNOLOGY in the year 1995 at
               Pulloor-626 115, Kariapatti Taluk , Virudhunagar District. Sethu Institute of Technology, the
@@ -31,91 +36,69 @@ const iqac = () => {
             </p>
 
           </div>
-          <div className="col-lg-6  mt-5 ml-auto">
-            <img src={banner1} alt="Images" class="img-fluid" />
+          <div className="col-lg-6 mt-5 ml-auto d-none d-md-block">
+            <img src={banner1} alt="Images" class="img-fluid" style={{ borderRadius: "15px" }} />
           </div>
         </div>
       </div>
-      <div className="container" style={{ marginTop: '50px' }}>
+      <div className="container" style={{ marginTop: '60px' }}>
         <div className="row mb-5 m-2">
-        <div className="col-lg-6  mt-5 ml-auto">
-            <img src={banner2} alt="Images" class="img-fluid" />
+          {/* <div className="col-lg-1"></div> */}
+          <div className="col-lg-4 mt-5 ml-auto">
+            <img src={banner2} alt="Images" class="img-fluid " width={1000} height={1000} style={{ borderRadius: "10px" }} />
           </div>
+          <div className="col-lg-2"></div>
           <div className="col-lg-6 mt-5 ml-auto">
             <div className='text-start mb-4 heading'>
-            <h3 className=" headingtext">NAAC</h3>
+              <h3 className=" headingtext">NAAC</h3>
             </div>
-            <p className="text-start">SETHU EDUCATIONAL TRUST established SETHU INSTITUTE OF TECHNOLOGY in the year 1995 at
-              Pulloor-626 115, Kariapatti Taluk , Virudhunagar District. Sethu Institute of Technology, the
-              brainchild of our Founder &amp; Chairman, Mr. S. Mohamed Jaleel, has been rendering its noble service
-              since 1995 catering to the sustained growth of the educationally, industrially and economically
-              backward rural areas.
+            <p className="text-start">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium nemo nesciunt consectetur dignissimos iste adipisci dicta vel ex eos dolores quam in omnis culpa tempore, consequatur nulla minima nam modi?
             </p>
-            <p>The college started with 180 students in the year 1995 with three UG
-              Programmes has recorded a stupendous growth of expanding to the strength of 4800 students with
-              16 UG Programmes and 5 Post Graduate Programmes.
-              <br />The College which is located on Madurai –
-              Tuticorin National Highway (NH 45B), Pulloor, Kariapatti, Virudhunagar District is just 12 KMs away
-              from Madurai airport. The campus is spread over a sprawling land of 132 acres providing a tranquil
-              and right learning ambience for aspiring students.
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias itaque id cupiditate quas, non quibusdam repudiandae atque animi ipsam maxime enim, fugit soluta tempora debitis, veritatis in nobis accusamus impedit.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias itaque id cupiditate quas, non quibusdam repudiandae atque animi ipsam maxime enim, fugit soluta tempora debitis, veritatis in nobis accusamus impedit.
             </p>
-
-          </div>
-          
-        </div>
-      </div>
-      <div className="container" style={{ marginTop: '50px' }}>
-        <div className="row mb-5 m-2">
-          <div className="col-lg-6 mt-5 ml-auto">
-            <div className='text-start mb-4 heading'>
-            <h3 className=" headingtext">About</h3>
-            </div>
-            <p className="text-start">SETHU EDUCATIONAL TRUST established SETHU INSTITUTE OF TECHNOLOGY in the year 1995 at
-              Pulloor-626 115, Kariapatti Taluk , Virudhunagar District. Sethu Institute of Technology, the
-              brainchild of our Founder &amp; Chairman, Mr. S. Mohamed Jaleel, has been rendering its noble service
-              since 1995 catering to the sustained growth of the educationally, industrially and economically
-              backward rural areas.
-            </p>
-            <p>The college started with 180 students in the year 1995 with three UG
-              Programmes has recorded a stupendous growth of expanding to the strength of 4800 students with
-              16 UG Programmes and 5 Post Graduate Programmes.
-              <br />The College which is located on Madurai –
-              Tuticorin National Highway (NH 45B), Pulloor, Kariapatti, Virudhunagar District is just 12 KMs away
-              from Madurai airport. The campus is spread over a sprawling land of 132 acres providing a tranquil
-              and right learning ambience for aspiring students.
-            </p>
-
-          </div>
-          <div className="col-lg-6  mt-5 ml-auto">
-            <img src={banner1} alt="Images" class="img-fluid" />
           </div>
         </div>
       </div>
-      <div className="container" style={{ marginTop: '50px' }}>
+      <div className="container" style={{ marginTop: '60px' }}>
         <div className="row mb-5 m-2">
+          <div className="col-lg-6 ml-auto d-lg-none">
+            <img src={banner3} alt="Images" class="img-fluid" style={{ borderRadius: "15px" }} />
+          </div>
           <div className="col-lg-6 mt-5 ml-auto">
-            <div className='text-start mb-4 heading'>
-            <h3 className=" headingtext">About</h3>
+            <div className='text-start mt-lg-5 mb-4 heading'>
+              <h3 className=" headingtext">Vision</h3>
             </div>
-            <p className="text-start">SETHU EDUCATIONAL TRUST established SETHU INSTITUTE OF TECHNOLOGY in the year 1995 at
-              Pulloor-626 115, Kariapatti Taluk , Virudhunagar District. Sethu Institute of Technology, the
-              brainchild of our Founder &amp; Chairman, Mr. S. Mohamed Jaleel, has been rendering its noble service
-              since 1995 catering to the sustained growth of the educationally, industrially and economically
-              backward rural areas.
+            <p className="text-start">To promote excellence in the technical education and scientific research for the benefit of the society
             </p>
-            <p>The college started with 180 students in the year 1995 with three UG
-              Programmes has recorded a stupendous growth of expanding to the strength of 4800 students with
-              16 UG Programmes and 5 Post Graduate Programmes.
-              <br />The College which is located on Madurai –
-              Tuticorin National Highway (NH 45B), Pulloor, Kariapatti, Virudhunagar District is just 12 KMs away
-              from Madurai airport. The campus is spread over a sprawling land of 132 acres providing a tranquil
-              and right learning ambience for aspiring students.
+          </div>
+          <div className="col-lg-6 mt-5 ml-auto d-none d-md-block">
+            <img src={banner3} alt="Images" class="img-fluid" style={{ borderRadius: "15px" }} />
+          </div>
+        </div>
+      </div>
+      <div className="container" style={{ marginTop: '60px' }}>
+        <div className="row mb-5 m-2">
+        <div className="col-lg-"></div>
+          <div className="col-lg-6  mt-5 ml-auto">
+            <img src={banner4} alt="Images" class="img-fluid" />
+          </div>
+          <div className="col-lg-6 mt-5 ml-auto">
+            <div className='text-start mb-4 heading mission-heading'>
+              <h3 className=" headingtext mission-headingtext">Mission</h3>
+            </div>
+            <p className="text-start">To provide quality technical education to fulfill the aspiration of the student and to meet the needsof the Industry
             </p>
+            <ul>
+              <li>To provide holistic learning ambience</li>
+              <li>To impart skills leading to employability and entrepreneurship</li>
+              <li>To establish effective linkage with Industries</li>
+              <li>To promote Research and Development activities</li>
+              <li>To offer services for the development of society through Education and Technology</li>
+            </ul>
 
           </div>
-          <div className="col-lg-6  mt-5 ml-auto">
-            <img src={banner1} alt="Images" class="img-fluid" />
-          </div>
+
         </div>
       </div>
     </>
--- src/Iqac/Navbar/Navbar.jsx
+++ src/Iqac/Navbar/Navbar.jsx
@@ -10,46 +10,64 @@ const Navbar = () => {
             {/* <!-- ======= Header ======= --> */}
             <header id="header" >
                 {/* <div className="col-12"></div> */}
-                <div>
-                    <nav className="navbar navbar-expand-lg py-4">
-                        <div className="container">
-                            <Link to={"https://sethu.ac.in/"} className="logo navbar-brand"><img src={logo} alt="logo" className='image-fluid' /></Link>
-                            <button className="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <span className="navbar-toggler-icon"></span>
-                            </button>
-                            <div className="collapse navbar-collapse " id="navbarSupportedContent">
-                                <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ">
-                                    <li className="nav-item mx-2 navActive text-decoration-none">
-                                        <Link to={'https://sethu.ac.in/'} className="nav-link" >Home</Link>
-                                    </li>
-                                    <li className="nav-item mx-2 navActive">
-                                        <Link to={'https://sethu.ac.in/best-engineering-colleges-in-madurai/'} className="nav-link" >College</Link>
-                                    </li>
-                                    <li className="nav-item mx-2 navActive">
-                                        <Link to={'https://sethu.ac.in/best-engineering-academy-in-madurai/'} className="nav-link" >Academics</Link>
-                                    </li>
-                                    <li className="nav-item mx-2 navActive">
-                                        <Link to={'https://sethu.ac.in/best-engineering-courses-in-madurai/'} className="nav-link" >Department</Link>
-                                    </li>
-                                    <li className="nav-item mx-2 navActive">
-                                        <Link to={'https://sethu.ac.in/best-placement-engineering-colleges-in-madurai/'} className="nav-link" >Placement</Link>
-                                    </li>
-                                    <li className="nav-item mx-2 navActive">
-                                        <Link to={'https://sethu.ac.in/research/'} className="nav-link" >Research</Link>
-                                    </li>
-                                </ul>
-                            </div>
+                <nav className="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0">
+                    <div className="container">
+                        <Link className='logo me-auto'><img src={logo} alt="logo" /></Link>
+                        {/* <Link className="navbar-brand" to="#!"><span style={{color: "#5e9693"}}>Psycho</span><span style={{color: "#fff"}}>logist</span></Link> */}
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                            <i className="fa fa-bars"></i>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto">
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="https://sethu.ac.in/">Home</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link navActive" to="https://sethu.ac.in/best-engineering-colleges-in-madurai/">College</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="https://sethu.ac.in/best-engineering-academy-in-madurai/">Academics</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="https://sethu.ac.in/best-engineering-courses-in-madurai/">Department</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="https://sethu.ac.in/best-placement-engineering-colleges-in-madurai/">Placement</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="https://sethu.ac.in/research/">Research</Link>
+                                </li>
+                            </ul>
+                            <ul className="navbar-nav d-flex flex-row">
+                                <li className="nav-item me-3 me-lg-0">
+                                    <Link className="nav-link" to="https://www.facebook.com/sethuinstitute/" target='_blank'>
+                                    <i className="fa fa-facebook"></i>
+                                    </Link>
+                                </li>
+                                <li className="nav-item me-3 me-lg-0">
+                                    <Link className="nav-link" to="https://www.instagram.com/sethuinstitute/" target='_blank'>
+                                        <i className="fa fa-instagram"></i>
+                                    </Link>
+                                </li>
+                                <li className="nav-item me-3 me-lg-0">
+                                    <Link className="nav-link" to="https://www.youtube.com/user/sethumedia" target='_blank'>
+                                        <i className="fa fa-youtube-play"></i>
+                                    </Link>
+                                </li>
+                            </ul>
                         </div>
-                    </nav>
-                </div>
-                
+                    </div>
+                </nav>
+
             </header>
             <div className='container d-flex justify-content-center banner-end'>
-                    <div className='text-center'>
+                <div className='text-center'>
                     <h3>IQAC</h3>
                     <p>Home {">"} IQAC</p>
-                    </div>
                 </div>
+            </div>
             {/* <!-- End Header --> */}
         </>
     )
